ae31bc9dc3d466d1e315fa221a04236aed6da617
Eyal Alsheich <eyal.als@gmail.com>
2023-01-01 14:47:29 +0200

    indentation

--- erigon/serviceRestart.sh
+++ erigon/serviceRestart.sh
@@ -6,8 +6,8 @@ colpur='\033[0;35m' # Purple
 colrst='\033[0m'    # Text Reset
 
 function llog() {
-        datestring=`date +"%Y-%m-%d %H:%M:%S"`
-        echo -e "$datestring - $@"
+	datestring=`date +"%Y-%m-%d %H:%M:%S"`
+	echo -e "$datestring - $@"
 }
 
 #initializing cli params
@@ -15,32 +15,34 @@ threshold=$1
 serviceName=$2
 tgApiToken=$3
 tgChatId=$4
+sendTelegram=true
 
 # checking cli params
 re='^[0-9]+$'
 if ! [[ $1 =~ $re ]]
 then
-        llog "${colred}[ERROR]${colrst} Threshold is required to be positive integer"
-        exit 1
+	llog "${colred}[ERROR]${colrst} Threshold is required to be positive integer"
+	exit 1
 fi
 
 if [[ -z "$serviceName" ]]
 then
-        llog "${colred}[ERROR]${colrst} Service Name is required"
-        exit 1
+	llog "${colred}[ERROR]${colrst} Service Name is required"
+	exit 1
 fi
 
 if systemctl --all --type service | grep -q "$serviceName"
 then
-    llog "${colgrn}[INFO]${colrst} Service $serviceName found"
+	llog "${colgrn}[INFO]${colrst} Service $serviceName found"
 else
-    llog "${colred}[ERROR]${colrst} Service $serviceName not found"
-    exit 1
+	llog "${colred}[ERROR]${colrst} Service $serviceName not found"
+	exit 1
 fi
 
 if [[ -z "$tgApiToken" || -z "$tgChatId" ]]
 then
-    llog "${colgrn}[INFO]${colrst} Telegram alerts will be skipped"
+	llog "${colgrn}[INFO]${colrst} Telegram alerts will be skipped"
+	sendTelegram=false
 fi
 
 # initializing vars
@@ -58,51 +60,51 @@ llog "${colgrn}[INFO]${colrst} Monitoring $serviceName"
 journalctl -f -u $serviceName -o cat -n 0 |
 while read line
 do
-    # ouputting some lines of the service log to see this script is alive
-    ((logAfterLinesCounter++))
-    if [[ $logAfterLinesCounter -gt $logAfterLines ]]
-    then
-        llog "${colgrn}[INFO]${colrst} $logAfterLines log lines processed ping"
-        logAfterLinesCounter=0
-    fi
+	# ouputting some lines of the service log to see this script is alive
+	((logAfterLinesCounter++))
+	if [[ $logAfterLinesCounter -gt $logAfterLines ]]
+	then
+		llog "${colgrn}[INFO]${colrst} $logAfterLines log lines processed ping"
+		logAfterLinesCounter=0
+	fi
 
-    # checking for the potential problem in the log
-    if [[ "$line" == *"$regexBody"* ]] || [[ "$line" == *"$regexHeader"* ]]
-    then
-        # get the block number
-        blockNumber=${line#*$searchStr}
-        llog "${colylw}[WARN]}${colrst} Triggered on block $blockNumber"
+	# checking for the potential problem in the log
+	if [[ "$line" == *"$regexBody"* ]] || [[ "$line" == *"$regexHeader"* ]]
+	then
+		# get the block number
+		blockNumber=${line#*$searchStr}
+		llog "${colylw}[WARN]}${colrst} Triggered on block $blockNumber"
 
-        # check if this is a repeat on this block number
-        if [[ "$blockNumber" == "$currentBlock" ]]
-        then
-                llog "${colylw}[WARN]${colrst} Repeat $repeat of $threshold detected on block $blockNumber"
-                ((repeat++))
+		# check if this is a repeat on this block number
+		if [[ "$blockNumber" == "$currentBlock" ]]
+		then
+			llog "${colylw}[WARN]${colrst} Repeat $repeat of $threshold detected on block $blockNumber"
+			((repeat++))
 
-                # if this repeats too many times we want to alert and restart the service
-                if [[ $repeat -gt $threshold ]]
-                then
-                        llog "${colpur}[INFO]${colrst} Restarting $serviceName !!"
-                        # reseting variables
-                        repeat=1
+			# if this repeats too many times we want to alert and restart the service
+			if [[ $repeat -gt $threshold ]]
+			then
+				llog "${colpur}[INFO]${colrst} Restarting $serviceName !!"
+				# reseting variables
+				repeat=1
 
-                        # restarting the service
-                        sudo systemctl restart $serviceName
+				# restarting the service
+				sudo systemctl restart $serviceName
 
-                        # alert using telegram if params were given
-                        if [[ ! -z "$tgApiToken" && ! -z "$tgChatId" ]]
-                        then
-                                llog "${colgrn}[INFO]${colrst} Sending telegram notification"
-                                curl -s -X POST https://api.telegram.org/bot$tgApiToken/sendMessage -d chat_id=$tgChatId -d text="$message $blockNumber"
-                        else
-                                llog "${colgrn}[INFO]${colrst} Skipping Telegram"
-                        fi
-                        llog "${colpur}[INFO]${colrst} Service $serviceName Restarted !!"
-                fi
-        else
-                # first time for this block so we set new current block and reset repeat counter
-                currentBlock=$blockNumber
-                repeat=1
-        fi
-    fi
+				# alert using telegram if params were given
+				if $sendTelegram
+				then
+					llog "${colgrn}[INFO]${colrst} Sending telegram notification"
+					curl -s -X POST https://api.telegram.org/bot$tgApiToken/sendMessage -d chat_id=$tgChatId -d text="$message $blockNumber"
+				else
+					llog "${colgrn}[INFO]${colrst} Skipping Telegram"
+				fi
+				llog "${colpur}[INFO]${colrst} Service $serviceName Restarted !!"
+			fi
+		else
+			# first time for this block so we set new current block and reset repeat counter
+			currentBlock=$blockNumber
+			repeat=1
+		fi
+	fi
 done
